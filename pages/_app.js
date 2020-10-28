cab4a147e6328effae11cc3edde3e22cecda2023
liuhuan086 <liuhuan086>
2020-10-28 15:36:41 +0800

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+// import '../styles/globals.css'
+//
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+//
+// export default MyApp
+
+import App from "next/app";
+import 'antd/dist/antd.css'
+
+export default App
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+ import React from 'react'
+import {Button} from "antd";
+
+const Home = () => (
+    <div>
+        <Head>
+            <title>Home</title>
+        </Head>
+        <div><Button>按钮</Button></div>
+    </div>
+)
+
+export default Home
